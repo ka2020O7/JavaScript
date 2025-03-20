519d09ab411133d105c08cb10cf4d1de5f44dfdf
kauan2020O7 <kauan_fernandes3@estudante.sesisenai.org.br>
2025-03-20 12:28:18 -0300

    update

--- scripts.js
+++ scripts.js
@@ -27,4 +27,63 @@ function calcular(numero1,numero2) {
 
 console .log (calcular(10,4))
 
-//////////////////////////////////////
+///////////////////////////////////
+
+let contador = 10;
+while (contador > 0) {
+    console.log(contador);
+    contador--;
+}
+
+///////////////////////////////////
+
+
+let original = "JavaScript";
+let reversed = original.split("").reverse().join("");
+console.log(reversed);
+
+
+///////////////////////////////////
+
+let str = "ContadorDeLetras";
+let n = str.length;
+console.log(n);
+
+
+//////////////////////////////////
+
+
+let meuCarro = new Object();
+meuCarro.fabricacao = "Ford";
+meuCarro.modelo = "Mustang";
+meuCarro.ano = 1969;
+
+console .log (meuCarro)
+
+
+//////////////////////////////////
+
+
+
+function mensagemPersonalizada(nome, mensagem = "Olá!") {
+    console.log(mensagem,nome);
+}
+
+mensagemPersonalizada("kauan");
+mensagemPersonalizada("kauan", "Bom dia!");
+
+
+//////////////////////////////////
+
+
+function media(n1, n2, n3){
+
+    media = (Number(n1) + Number(n2) + Number(n3))/3 ;
+    console.log(media);
+    }
+    media(prompt("N1"), prompt("N2: "), prompt("N3: "));
+
+
+///////////////////////////////////
+
+
